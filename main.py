6f5fe281775eabef0c4b998d1079641dcd7da389
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-05-08 16:48:55 -0400

    Customization Function
    Started with Customization Function

--- main.py
+++ main.py
@@ -6,4 +6,20 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset, random_split
 
-print("Hello World")
+DATA_FILENAME = "carData.csv"
+dataframe_raw = pd.read_csv(DATA_FILENAME)
+dataframe_raw.head()
+
+your_name = "Johaan Riat"
+def customize_dataset(dataframe_raw, rand_str):
+    dataframe = dataframe_raw.copy(deep=True)
+
+    #drop rows
+    dataframe = dataframe.sample(int(.95*len(dataframe)))
+    random_state=int(ord(rand_str[0]))
+
+    #scale input
+    dataframe.Year = dataframe.Year * ord(rand_str[1])/100
+
+    #scale target
+    dataframe.MSRP = dataframe.MSRP * ord(rand_str[2])/100
