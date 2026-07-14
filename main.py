b9bb41660d6d106115690b2865645d4c8fca6131
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-07-14 18:15:47 -0400

    Restarting development of project

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-import torch
-#import jovian
-import torch.nn as nn
-import pandas as pd
-import matplotlib.pyplot as plt
-import torch.nn.functional as F
-from torch.utils.data import DataLoader, TensorDataset, random_split
 
-DATA_FILENAME = "carData.csv"
-dataframe_raw = pd.read_csv(DATA_FILENAME)
-dataframe_raw.head()
 
-your_name = "Johaan Riat"
-def customize_dataset(dataframe_raw, rand_str):
-    dataframe = dataframe_raw.copy(deep=True)
-
-    #drop rows
-    dataframe = dataframe.sample(int(.95*len(dataframe)))
-    random_state=int(ord(rand_str[0]))
-
-    #scale input
-    dataframe.Year = dataframe.Year * ord(rand_str[1])/100
-
-    #scale target
-    dataframe.MSRP = dataframe.MSRP * ord(rand_str[2])/100
+print("Hello World!")
