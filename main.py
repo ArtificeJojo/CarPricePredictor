d717fb5d3826d4559bd2af5a918a98265908c09c
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-07-14 20:26:39 -0400

    Added Data Formatting
    Formatted data
    Added method to convert price to integer

--- main.py
+++ main.py
@@ -3,5 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy as sp
+from contourpy.util import data
 
-print("Hello World!")
+
+#Read csv file and format data
+def LoadData():
+    dataTable = pd.read_csv('carData.csv')
+    dataTable = dataTable.iloc[:]
+    headers = ["Brand", "Name", "Year", "Market Price", "Current Price", "Miles",
+               "Body Style", "Engine", "Fuel", "Transmission", "Owners", "Damage", ""]
+    dataTable.columns = headers
+
+    isMissing = dataTable
+    isMissing.isna().any()
+    isMissing.isnull().any()
+
+    return dataTable
+
+def PriceToInt(d):
+    d['Market Price'] = d['Market Price'].astype(int)
+    return d
+def main():
+    data = LoadData()
+
+    data = PriceToInt(data)
+
+    print(data.head())
+
+main()
